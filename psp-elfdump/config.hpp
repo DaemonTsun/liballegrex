e42974d1c68f4d695b2d5ec01b99b6b0a0120abd
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-04-06 11:11:20 -0400

    Add command line options to disassemble range
    Move config.hpp to psp-elfdump
    Bump up version to 0.9

--- psp-elfdump/config.hpp
+++ psp-elfdump/config.hpp
@@ -2,4 +2,4 @@
 
 #define PSP_ELFDUMP_NAME "psp-elfdump"
 #define PSP_ELFDUMP_AUTHOR "DaemonTsun"
-#define PSP_ELFDUMP_VERSION "0.1"
+#define PSP_ELFDUMP_VERSION "0.9"
--- psp-elfdump/main.cpp
+++ psp-elfdump/main.cpp
@@ -11,7 +11,10 @@
 #include "memory_stream.hpp"
 #include "string.hpp"
 #include "number_types.hpp"
-#include "config.hpp"
+
+#include "psp-elfdump/config.hpp"
+
+#define INFER_ADDR UINT32_MAX
 
 struct arguments
 {
@@ -22,6 +25,8 @@ struct arguments
     std::string decrypted_elf_output; // --dump-decrypt
     bool emit_pseudo;        // -p, --pseudo
     u32 vaddr;               // -a, --vaddr
+    u32 start_pos;           // -A, --start
+    u32 end_pos;             // -B, --end
     bool verbose;            // -v, --verbose
 
     std::string input_file;
@@ -35,10 +40,8 @@ const arguments default_arguments{
     .decrypted_elf_output = "",
     .emit_pseudo = false,
     .vaddr = INFER_VADDR,
-    /* TODO: implement these
-    .start_pos = INFER_ADDR, // when specified, only disassemble the given range
+    .start_pos = INFER_ADDR,
     .end_pos = INFER_ADDR,
-    */
     .verbose = false,
     .input_file = ""
 };
@@ -51,20 +54,25 @@ void print_usage()
          "by " PSP_ELFDUMP_AUTHOR "\n"
          "\n"
          "Optional arguments:\n"
-         " -h, --help                  show this help and exit\n"
-         " -g, --glabel                emit \"glabel name\" for global labels\n"
-         " -o OUTPUT, --output OUTPUT  output filename (default: stdout)\n"
-         " --log LOGFILE               output all information messages to LOGFILE (stdout by default)\n"
-         " -s, --section NAME          disassemble the section with name NAME (\".text\" by default)\n"
-         " --dump-decrypt OUTPUT       optional output file to dump the decrypted ELF to.\n"
-         "                             if set, only dumps the ELF to OUTPUT and exits.\n" 
-         " -p, --pseudo                emit pseudoinstructions for related instructions\n"
-         " -a VADDR, --vaddr VADDR     virtual address of the first instruction\n"
-         "                             will be read from elf instead if not set\n"
-         " -v, --verbose               verbose progress output\n"
+         "  -h, --help                  show this help and exit\n"
+         "  -g, --glabel                emit \"glabel name\" for global labels\n"
+         "  -o OUTPUT, --output OUTPUT  output filename (default: stdout)\n"
+         "  --log LOGFILE               output all information messages to LOGFILE (stdout by default)\n"
+         "  -s, --section NAME          disassemble the section with name NAME (\".text\" by default)\n"
+         "  --dump-decrypt OUTPUT       optional output file to dump the decrypted ELF to.\n"
+         "                              if set, only dumps the ELF to OUTPUT and exits.\n" 
+         "  -p, --pseudo                emit pseudoinstructions for related instructions\n"
+         "  -a VADDR, --vaddr VADDR     virtual address of the first instruction\n"
+         "                              will be read from elf instead if not set\n"
+         "  -A OFFSET, --start OFFSET   if set, only disassemble MIPS starting from\n"
+         "                              OFFSET and exit (ignores all ELF data).\n" 
+         "  -B OFFSET, --end OFFSET     if set, only disassemble MIPS up to OFFSET\n"
+         "                              and exit. only works with -A, must not be\n"
+         "                              smaller than -A's OFFSET\n"
+         "  -v, --verbose               verbose progress output\n"
          "\n"
          "Arguments:\n"
-         " OBJFILE      ELF object file to disassemble the given section for\n"
+         "  OBJFILE      ELF object file to disassemble the given section for\n"
          );
 }
 
@@ -139,7 +147,27 @@ void parse_arguments(int argc, const char **argv, arguments *out)
             if (i >= argc - 1)
                 throw std::runtime_error(str(arg, " expects a positional argument: the VADDR"));
 
-            out->vaddr = std::stoul(argv[i+1]);
+            out->vaddr = std::stoul(argv[i+1], nullptr, 0);
+            i += 2;
+            continue;
+        }
+
+        if (arg == "-A" || arg == "--start")
+        {
+            if (i >= argc - 1)
+                throw std::runtime_error(str(arg, " expects a positional argument: the offset"));
+
+            out->start_pos = std::stoul(argv[i+1], nullptr, 0);
+            i += 2;
+            continue;
+        }
+
+        if (arg == "-B" || arg == "--end")
+        {
+            if (i >= argc - 1)
+                throw std::runtime_error(str(arg, " expects a positional argument: the offset"));
+
+            out->end_pos = std::stoul(argv[i+1], nullptr, 0);
             i += 2;
             continue;
         }
@@ -232,6 +260,46 @@ void dump_decrypted_elf(file_stream *in, file_stream *log, const arguments &args
     printf("dumped decrypted ELF from %s to %s\n", args.input_file.c_str(), args.decrypted_elf_output.c_str());
 }
 
+void disassemble_range(file_stream *in, file_stream *log, const arguments &args)
+{
+    u32 from = args.start_pos;
+    u32 to = args.end_pos;
+    u32 sz = to - from;
+
+    if (from > to)
+        throw std::runtime_error(str("start offset ", from, " is larger than end offset ", to));
+
+    if (to > in->size())
+        throw std::runtime_error(str("end offset ", to, " is larger than input file size ", in->size()));
+
+    parse_config pconf;
+    pconf.log = log;
+    pconf.vaddr = args.vaddr;
+    pconf.verbose = args.verbose;
+    pconf.emit_pseudo = args.emit_pseudo;
+
+    if (pconf.vaddr == INFER_VADDR)
+        pconf.vaddr = 0;
+
+    auto memstr = memory_stream(sz);
+    in->read_at(memstr.data(), from, sz);
+
+    parse_data pdata;
+    parse_allegrex(&memstr, &pconf, &pdata);
+
+    FILE *outfd = stdout;
+
+    if (!args.output_file.empty())
+        outfd = fopen(args.output_file.c_str(), "w");
+
+    file_stream out(outfd);
+
+    if (!out)
+        throw std::runtime_error("could not open output file");
+
+    print_instructions(&out, pdata.instructions);
+}
+
 int main(int argc, const char **argv)
 try
 {
@@ -269,8 +337,13 @@ try
         return 0;
     }
 
-    disassemble_elf(&in, &log, args);
+    if (args.start_pos != INFER_ADDR || args.end_pos != INFER_ADDR)
+    {
+        disassemble_range(&in, &log, args);
+        return 0;
+    }
 
+    disassemble_elf(&in, &log, args);
     return 0;
 }
 catch (std::runtime_error &e)
