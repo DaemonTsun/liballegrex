58f31c7899af5377c5eb60621ba757a4f7c8c9b1
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-04-16 07:29:12 -0400

    Add srl & rotr tests
    Rename text_fixed.cpp to test_fixed.cpp

--- tests/test_common.hpp
+++ tests/test_common.hpp
@@ -18,12 +18,36 @@
     parse_data pdata;
     
 #define parse_opcode(OPCODE) \
+    inst.opcode = OPCODE;\
     parse_instruction(OPCODE, &inst, &conf, &pdata);
     
 #define assert_mnemonic(MNEM) \
     assert_equal(inst.mnemonic, allegrex_mnemonic::MNEM);
+    
+#define assert_argument_count(N) \
+    assert_equal(inst.arguments.size(), N);
+    
+#define assert_argument_type(N, T) \
+    assert_equal(std::holds_alternative<T>(inst.arguments.at(N)), true);
+    
+#define _assert_argument_equals(N, T, V) \
+    assert_equal(std::get<T>(inst.arguments.at(N)), V);
+    
+#define _assert_argument_data_equals(N, T, V) \
+    assert_equal(std::get<T>(inst.arguments.at(N)).data, V);
+    
+#define assert_argument_mips_register(N, R) \
+    _assert_argument_equals(N, mips_register, R)
+    
+#define assert_argument_shift(N, S) \
+    _assert_argument_data_equals(N, shift, S)
 
 std::ostream &operator<<(std::ostream &lhs, allegrex_mnemonic rhs)
 {
     return lhs << get_mnemonic_name(rhs);
+}
+
+std::ostream &operator<<(std::ostream &lhs, mips_register rhs)
+{
+    return lhs << register_name(rhs);
 }
---
+++ tests/test_srlrotr.cpp
@@ -0,0 +1,101 @@
+
+#include <t1/t1.hpp>
+#include "tests/test_common.hpp"
+
+// srl
+define_test(srl_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x00000002);
+
+    assert_mnemonic(SRL);
+    assert_argument_count(3);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_mips_register(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_register);
+    assert_argument_mips_register(1, mips_register::ZERO);
+
+    assert_argument_type(2, shift);
+    assert_argument_shift(2, 0u);
+}
+
+define_test(srl_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x000008C2);
+
+    assert_mnemonic(SRL);
+    assert_argument_count(3);
+
+    assert_argument_mips_register(0, mips_register::AT);
+    assert_argument_mips_register(1, mips_register::ZERO);
+    assert_argument_shift(2, 3u);
+}
+
+define_test(srl_2)
+{
+    setup_test_variables();
+
+    parse_opcode(0x001fffc2);
+
+    assert_mnemonic(SRL);
+    assert_argument_count(3);
+
+    assert_argument_mips_register(0, mips_register::RA);
+    assert_argument_mips_register(1, mips_register::RA);
+    assert_argument_shift(2, 31u);
+}
+
+// rotr
+define_test(rotr_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x00200002);
+
+    assert_mnemonic(ROTR);
+    assert_argument_count(3);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_mips_register(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_register);
+    assert_argument_mips_register(1, mips_register::ZERO);
+
+    assert_argument_type(2, shift);
+    assert_argument_shift(2, 0u);
+}
+
+define_test(rotr_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x002008C2);
+
+    assert_mnemonic(ROTR);
+    assert_argument_count(3);
+
+    assert_argument_mips_register(0, mips_register::AT);
+    assert_argument_mips_register(1, mips_register::ZERO);
+    assert_argument_shift(2, 3u);
+}
+
+define_test(rotr_2)
+{
+    setup_test_variables();
+
+    parse_opcode(0x003fffc2);
+
+    assert_mnemonic(ROTR);
+    assert_argument_count(3);
+
+    assert_argument_mips_register(0, mips_register::RA);
+    assert_argument_mips_register(1, mips_register::RA);
+    assert_argument_shift(2, 31u);
+}
+
+define_default_test_main();
