d5f67b491f4db79f7e006b36d8c4b4542713d0e2
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-04-22 08:02:21 -0400

    Add tests for mfc1, cfc1, mtc1, ctc1

---
+++ tests/test_cop1.cpp
@@ -0,0 +1,128 @@
+#include <t1/t1.hpp>
+#include "tests/test_common.hpp"
+
+// mfc1 rt, fs
+define_test(mfc1_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44000000);
+    assert_mnemonic(MFC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(mfc1_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44030800);
+    assert_mnemonic(MFC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::V1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F1);
+}
+
+// cfc1 rt, fs
+define_test(cfc1_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44400000);
+    assert_mnemonic(CFC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(cfc1_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44430800);
+    assert_mnemonic(CFC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::V1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F1);
+}
+
+// mtc1 rt, fs
+define_test(mtc1_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44800000);
+    assert_mnemonic(MTC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(mtc1_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44830800);
+    assert_mnemonic(MTC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::V1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F1);
+}
+
+// ctc1 rt, fs
+define_test(ctc1_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44c00000);
+    assert_mnemonic(CTC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::ZERO);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(ctc1_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x44c30800);
+    assert_mnemonic(CTC1);
+    assert_argument_count(2);
+
+    assert_argument_type(0, mips_register);
+    assert_argument_equals(0, mips_register::V1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F1);
+}
+
+define_default_test_main();
