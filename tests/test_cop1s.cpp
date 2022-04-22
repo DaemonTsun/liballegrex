c4fd73c11f89b495d7195d8728b88a71180641f5
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-04-22 07:46:23 -0400

    Add tests for c.fmt.s

--- tests/test_cop1s.cpp
+++ tests/test_cop1s.cpp
@@ -391,4 +391,500 @@ define_test(dis_int)
     assert_argument_count(0);
 }
 
+// c.f.s fs, ft
+define_test(c_f_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000030);
+    assert_mnemonic(C_F_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_f_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030830);
+    assert_mnemonic(C_F_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.un.s fs, ft
+define_test(c_un_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000031);
+    assert_mnemonic(C_UN_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_un_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030831);
+    assert_mnemonic(C_UN_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.eq.s fs, ft
+define_test(c_eq_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000032);
+    assert_mnemonic(C_EQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_eq_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030832);
+    assert_mnemonic(C_EQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ueq.s fs, ft
+define_test(c_ueq_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000033);
+    assert_mnemonic(C_UEQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ueq_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030833);
+    assert_mnemonic(C_UEQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.olt.s fs, ft
+define_test(c_olt_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000034);
+    assert_mnemonic(C_OLT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_olt_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030834);
+    assert_mnemonic(C_OLT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ult.s fs, ft
+define_test(c_ult_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000035);
+    assert_mnemonic(C_ULT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ult_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030835);
+    assert_mnemonic(C_ULT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ole.s fs, ft
+define_test(c_ole_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000036);
+    assert_mnemonic(C_OLE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ole_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030836);
+    assert_mnemonic(C_OLE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ule.s fs, ft
+define_test(c_ule_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000037);
+    assert_mnemonic(C_ULE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ule_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030837);
+    assert_mnemonic(C_ULE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.sf.s fs, ft
+define_test(c_sf_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000038);
+    assert_mnemonic(C_SF_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_sf_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030838);
+    assert_mnemonic(C_SF_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ngle.s fs, ft
+define_test(c_ngle_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46000039);
+    assert_mnemonic(C_NGLE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ngle_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x46030839);
+    assert_mnemonic(C_NGLE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.seq.s fs, ft
+define_test(c_seq_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003a);
+    assert_mnemonic(C_SEQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_seq_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083a);
+    assert_mnemonic(C_SEQ_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ngl.s fs, ft
+define_test(c_ngl_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003b);
+    assert_mnemonic(C_NGL_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ngl_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083b);
+    assert_mnemonic(C_NGL_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.lt.s fs, ft
+define_test(c_lt_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003c);
+    assert_mnemonic(C_LT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_lt_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083c);
+    assert_mnemonic(C_LT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.nge.s fs, ft
+define_test(c_nge_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003d);
+    assert_mnemonic(C_NGE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_nge_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083d);
+    assert_mnemonic(C_NGE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.le.s fs, ft
+define_test(c_le_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003e);
+    assert_mnemonic(C_LE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_le_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083e);
+    assert_mnemonic(C_LE_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
+// c.ngt.s fs, ft
+define_test(c_ngt_s_0)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4600003f);
+    assert_mnemonic(C_NGT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F0);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F0);
+}
+
+define_test(c_ngt_s_1)
+{
+    setup_test_variables();
+
+    parse_opcode(0x4603083f);
+    assert_mnemonic(C_NGT_S);
+    assert_argument_non_extra_count(2);
+
+    assert_argument_type(0, mips_fpu_register);
+    assert_argument_equals(0, mips_fpu_register::F1);
+
+    assert_argument_type(1, mips_fpu_register);
+    assert_argument_equals(1, mips_fpu_register::F3);
+}
+
 define_default_test_main();
