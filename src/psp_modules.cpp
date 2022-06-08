598e93d8f911683256543adf090b5da9059bf4df
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2022-06-08 11:27:31 -0400

    Add psp module utility functions

--- src/psp_modules.cpp
+++ src/psp_modules.cpp
@@ -1,6 +1,8 @@
 
 #include <array>
-#include <vector>
+
+#include <stdio.h>
+#include <string.h>
 
 #include "internal/map_macro.hpp"
 #include "internal/psp_module_function_argument_defs.hpp"
@@ -22,11 +24,6 @@ static_assert(ARGS(a, b, c) == L"abc");
 
 #define NO_ARGS L""
 
-const char *get_psp_function_arg_name(psp_function_arg_t arg)
-{
-    return _get_psp_function_arg_name(arg);
-}
-
 #include "internal/psp_modules.inl"
 
 constexpr psp_function unknown_function{
@@ -45,6 +42,60 @@ const psp_module *get_psp_modules()
 u32 get_psp_module_count()
 {
     return _modules.size();
+}
+
+const psp_module *get_psp_module_by_name(const char *mod)
+{
+    // could be optimized
+    for (const psp_module &md : _modules)
+        if (strcmp(md.name, mod) == 0)
+            return &md;
+
+    return nullptr;
+}
+
+const psp_function *get_psp_function_by_nid(const char *mod, u32 nid)
+{
+    const psp_module *md = get_psp_module_by_name(mod);
+
+    if (md == nullptr)
+        return nullptr;
+
+    // could be optimized even more
+    for (int i = 0; i < md->function_count; ++i)
+    {
+        const psp_function *f = md->functions + i;
+
+        if (f->nid == nid)
+            return f;
+    }
+
+    return nullptr;
+}
+
+const psp_function *get_psp_function_by_name(const char *mod, const char *name)
+{
+    const psp_module *md = get_psp_module_by_name(mod);
+
+    if (md == nullptr)
+        return nullptr;
+
+    char nidname[256];
+
+    for (int i = 0; i < md->function_count; ++i)
+    {
+        const psp_function *f = md->functions + i;
+
+        if (strcmp(f->name, name) == 0)
+            return f;
+
+        get_psp_function_nid_name(f, nidname);
+
+        if (strcmp(f->name, nidname) == 0)
+            return f;
+    }
+
+    return nullptr;
 }
 
 const psp_function *get_psp_function(u16 mod, u16 fun)
@@ -71,4 +122,15 @@ const char *get_psp_module_name(u16 mod)
 const char *get_psp_function_name(u16 mod, u16 fun)
 {
     return get_psp_function(mod, fun)->name;
+}
+
+void get_psp_function_nid_name(const psp_function *fun, char *dst, u32 sz)
+{
+    const psp_module *md = &_modules.at(fun->module_num);
+    snprintf(dst, sz, "%s_%08X", md->name, fun->nid);
+}
+
+const char *get_psp_function_arg_name(psp_function_arg_t arg)
+{
+    return _get_psp_function_arg_name(arg);
 }
--- src/psp_modules.hpp
+++ src/psp_modules.hpp
@@ -30,10 +30,13 @@ struct psp_module
 const psp_module *get_psp_modules();
 u32 get_psp_module_count();
 
-// TODO: get function by nid & (optional) module
-// TODO: get function by name & (optional) module
+const psp_module *get_psp_module_by_name(const char *mod);
+const psp_function *get_psp_function_by_nid(const char *mod, u32 nid);
+const psp_function *get_psp_function_by_name(const char *mod, const char *name);
+
 const psp_function *get_psp_function(u16 mod, u16 fun);
 const char *get_psp_module_name(u16 mod);
 const char *get_psp_function_name(u16 mod, u16 fun);
 
+void get_psp_function_nid_name(const psp_function *fun, char *dst, u32 sz = -1);
 const char *get_psp_function_arg_name(psp_function_arg_t arg);
